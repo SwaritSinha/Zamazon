f7fd0bebf3928cff39470a5daa5ab9ae3b254f58
SwaritSinha <90327506+SwaritSinha@users.noreply.github.com>
2022-01-04 16:11:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+Alert = document.getElementById('btnAlert');
+
+btnAlert.addEventListener('click', alertFunction);
+
+function alertFunction() {
+  alert('Conratulations bought a random gift')
+  alert('Will arrive in 10 days')
+}
